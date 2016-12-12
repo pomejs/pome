a7ef98fd901956198aa25952c1982dcc0f3003c5
ference <fym201@msn.com>
2016-12-12 18:15:32 +0800

    update template to use express 4.x

--- template/web-server/app.js
+++ template/web-server/app.js
@@ -1,26 +1,30 @@
 var express = require('express');
-var app = express.createServer();
+var bodyParser = require('body-parser');
+var errorhandler = require('errorhandler');
+var methodOverride = require('method-override');
 
-app.configure(function(){
-  app.use(express.methodOverride());
-  app.use(express.bodyParser());
-  app.use(app.router);
-  app.set('view engine', 'jade');
-  app.set('views', __dirname + '/public');
-  app.set('view options', {layout: false});
-  app.set('basepath',__dirname + '/public');
-});
+var app = express();
 
-app.configure('development', function(){
+app.set('view engine', 'html');
+app.set('views', __dirname + '/public');
+app.set('view options', {layout: false});
+app.set('basepath',__dirname + '/public');
+
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+if(process.env.NODE_ENV === 'development'){
   app.use(express.static(__dirname + '/public'));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
+  app.use(errorhandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+} else {
   var oneYear = 31557600000;
   app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-  app.use(express.errorHandler());
-});
+  app.use(errorhandler());
+}
 
 console.log("Web server has started.\nPlease log on http://127.0.0.1:3001/index.html");
 
